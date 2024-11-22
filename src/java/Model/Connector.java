c7e8e3701a646b5e5efbdfc28f5ab16de6496753
duyd486 <duyd486@gmail.com>
2024-11-22 14:48:40 +0700

    Connector-code

--- src/java/Model/Connector.java
+++ src/java/Model/Connector.java
@@ -4,10 +4,31 @@
  */
 package Model;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 /**
  *
  * @author tomng
  */
 public class Connector {
-    
+    public static Connection getConnection() {
+        Connection connect = null;
+
+        try {
+          // Class.forName("com.mysql.jdbc.Driver");
+          Class.forName("com.mysql.cj.jdbc.Driver");
+          connect = DriverManager.getConnection(
+              "jdbc:mysql://localhost:3306/duongduaeducation",
+              "root",
+              "");
+
+          System.out.println("Database connected.");
+          return connect;
+
+        } catch (Exception e) {
+          // e.printStackTrace();
+          return connect;
+        }
+    }
 }
